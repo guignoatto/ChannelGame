9a84296dc2f0306372916087618469217dc51a14
Rafael <fazerafael@gmail.com>
2023-01-17 17:39:53 -0300

    WWise changes, Sound menu
    Implemented sound menu with WWise

--- ChannelGame/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ ChannelGame/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -18,6 +18,13 @@ namespace AK
         static const AkUniqueID SHOT_MACHINEGUN = 4191302951U;
     } // namespace EVENTS
 
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID MASTERVOLUME = 2918011349U;
+        static const AkUniqueID MUSICVOLUME = 2346531308U;
+        static const AkUniqueID SFXVOLUME = 988953028U;
+    } // namespace GAME_PARAMETERS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
@@ -27,6 +34,8 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
