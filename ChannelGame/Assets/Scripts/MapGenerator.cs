cdbc71e3d6effc3dd72790063788f12a668609d9
Rafael <fazerafael@gmail.com>
2023-05-29 15:18:47 -0300

    New Chunks
    Maade new chunks using Siomons's assets and pallets

--- ChannelGame/Assets/Scripts/MapGenerator.cs
+++ ChannelGame/Assets/Scripts/MapGenerator.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,6 +12,8 @@ public class MapGenerator : MonoBehaviour
     public int mapHeight = 6; // the number of chunks to generate vertically
     public int numSpecialChunks = 2; // the number of special chunks to generate
 
+    private List<Vector2Int> occupiedPositions; // list of occupied positions by special chunks
+
     void Start()
     {
         GenerateMap();
@@ -22,17 +23,9 @@ public class MapGenerator : MonoBehaviour
     {
         Vector3 mapCenter = new Vector3((mapWidth - 1) * chunkWidth / 2f, (mapHeight - 1) * chunkHeight / 2f, 0);
 
-        // Generate all the regular chunks
-        for (int x = 0; x < mapWidth; x++)
-        {
-            for (int y = 0; y < mapHeight; y++)
-            {
-                GameObject newChunk = Instantiate(chunks[Random.Range(0, chunks.Length)], new Vector3(x * chunkWidth, y * chunkHeight, 0) - mapCenter, Quaternion.identity);
-                newChunk.transform.parent = transform;
-            }
-        }
+        occupiedPositions = new List<Vector2Int>();
 
-        // Generate the special chunks
+        // Generate the special chunks first
         for (int i = 0; i < numSpecialChunks; i++)
         {
             int randomX;
@@ -46,22 +39,36 @@ public class MapGenerator : MonoBehaviour
 
                 newSpecialChunk = specialChunks[Random.Range(0, specialChunks.Length)];
             }
-            while (IsChunkSpecial(newSpecialChunk));
+            while (IsPositionOccupied(randomX, randomY));
 
             GameObject instantiatedSpecialChunk = Instantiate(newSpecialChunk, new Vector3(randomX * chunkWidth, randomY * chunkHeight, 0) - mapCenter, Quaternion.identity);
             instantiatedSpecialChunk.transform.parent = transform;
+
+            // Mark the position of the special chunk as occupied
+            occupiedPositions.Add(new Vector2Int(randomX, randomY));
+        }
+
+        // Generate the regular chunks, skipping the occupied positions
+        for (int x = 0; x < mapWidth; x++)
+        {
+            for (int y = 0; y < mapHeight; y++)
+            {
+                if (!IsPositionOccupied(x, y))
+                {
+                    GameObject newChunk = Instantiate(chunks[Random.Range(0, chunks.Length)], new Vector3(x * chunkWidth, y * chunkHeight, 0) - mapCenter, Quaternion.identity);
+                    newChunk.transform.parent = transform;
+                }
+            }
         }
     }
 
-    bool IsChunkSpecial(GameObject chunk)
+    bool IsPositionOccupied(int x, int y)
     {
-        GameObject[] specialChunks = GameObject.FindGameObjectsWithTag("SpecialChunk");
-        foreach (GameObject specialChunk in specialChunks)
+        foreach (Vector2Int position in occupiedPositions)
         {
-            if (specialChunk.name == chunk.name)
+            if (position.x == x && position.y == y)
                 return true;
         }
         return false;
     }
 }
-
